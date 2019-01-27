4a0161ca23980f0a846b429084d13b05a36c9afe
Miodrag Radosavljević <cinnamontress@gmail.com>
2019-01-27 18:38:55 +0100

    init with five simple pages

---
+++ resources/views/services.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Five pages exercise</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);
+            color: #333;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #6369F3;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .page {
+            font-size: 18px;
+            margin: 100px 0;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+
+    <div class="content">
+        <div class="title m-b-md">
+            Five pages
+        </div>
+
+        <div class="links">
+            <a href="/">Homepage</a>
+            <a href="/products">Products</a>
+            <a href="/services">Services</a>
+            <a href="/contact">Contact</a>
+            <a href="/about">About</a>
+        </div>
+
+        <div class="page">
+            <p>Services are currently unavailable.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
